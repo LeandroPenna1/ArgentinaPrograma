f14d925a06c4f4973b2b31c2d7aaae667ceff452
LeandroPenna1 <134015680+LeandroPenna1@users.noreply.github.com>
2023-05-28 15:47:45 -0300

    Agrego el archivo conversionGrados.js

---
+++ Practico0/conversionGrados.js
@@ -0,0 +1,9 @@
+//Ejercicio A
+fahrenheit = 50;
+celsius = ((fahrenheit - 32 * 5/9))
+console.log(celsius + " C")
+
+//Ejercicio B
+fahrenheit = 75;
+celsius = ((fahrenheit - 32 * 5/9))
+console.log(celsius + " C")
