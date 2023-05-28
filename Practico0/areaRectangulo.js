f1cb7cbb93fef73d212e8f765d27a8c669ebba4a
LeandroPenna1 <134015680+LeandroPenna1@users.noreply.github.com>
2023-05-28 15:56:25 -0300

    Practico N0 finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+// Ejercicio A. Base 5cm, Altura 3cm
+Base = 5;
+Altura = 3;
+
+console.log("Respuesta ejercicio A: " + Base * Altura)
+
+//Ejercicio B. Base 12.5, Altura 5.3cm
+Base = 12.5;
+Altura = 5.3;
+
+console.log("Respuesta ejercicio B: " + Base * Altura)
---
+++ Practico0/promedio3.js
@@ -0,0 +1,20 @@
+//Ejercicio A
+var1 = 5;
+var2 = 7;
+var3 = 9;
+
+console.log((var1 + var2 + var3) / 3)
+
+//Ejercicio B
+var1 = -2;
+var2 = -3;
+var3 = -8;
+
+console.log((var1 + var2 + var3) / 3)
+
+//Ejercicio C
+var1 = -1.2;
+var2 = 5.3;
+var3 = -3.3;
+
+console.log((var1 + var2 + var3) / 3)
