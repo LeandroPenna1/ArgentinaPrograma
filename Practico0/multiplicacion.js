90472bafdcbdd63bd4c8a813cc4a38e8ca22aafd
LeandroPenna1 <134015680+LeandroPenna1@users.noreply.github.com>
2023-05-28 15:10:56 -0300

    Agrega archivos resta y multiplicacion

---
+++ Practico0/multiplicacion.js
@@ -0,0 +1,7 @@
+var1 = 2;
+var2 = 17;
+var3 = 6;
+
+console.log(var1 * var2 * var3)
+
+// La respuesta es 204 (multiplicación entre los tres números dados en el enunciado)
---
+++ Practico0/resta.js
@@ -0,0 +1,6 @@
+var1 = 10;
+var2 = 2;
+
+console.log(var1 - var2)
+
+// La respuesta es 8.
