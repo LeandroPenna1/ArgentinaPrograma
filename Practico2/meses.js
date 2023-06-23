7815d0ad25476fbde9f4f37514b9b5e5d2fe1b40
LeandroPenna1 <134015680+LeandroPenna1@users.noreply.github.com>
2023-06-22 21:26:37 -0300

    meses

---
+++ Practico2/meses.js
@@ -0,0 +1,28 @@
+const readlineSync = require('readline-sync');
+let numeroMes = readlineSync.question("Ingresa un numero:\n");
+
+if(numeroMes = 1){
+    console.log("La cantidad de dias del mes de Enero es 31")
+} else if(numeroMes = 2){
+    console.log("La cantidad de dias del mes de Febrero es 28")
+} else if(numeroMes = 3){
+    console.log("La cantidad de dias del mes de Marzo es 31")
+} else if(numeroMes = 4){
+    console.log("La cantidad de dias del mes de Abril es 30")
+} else if(numeroMes = 5){
+    console.log("La cantidad de dias del mes de Mayo es 31")
+} else if(numeroMes = 6){
+    console.log("La cantidad de dias del mes de Junio es 30")
+} else if(numeroMes = 7){
+    console.log("La cantidad de dias del mes de Julio es 31")
+} else if(numeroMes = 8){
+    console.log("La cantidad de dias del mes de Agosto es 31")
+} else if(numeroMes = 9){
+    console.log("La cantidad de dias del mes de Septiembre es 30")
+} else if(numeroMes = 10){
+    console.log("La cantidad de dias del mes de Octubre es 31")
+} else if(numeroMes = 11){
+    console.log("La cantidad de dias del mes de Noviembre es 30")
+} else if(numeroMes = 12){
+    console.log("La cantidad de dias del mes de Diciembre es 31")
+}
